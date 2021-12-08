68649748b416519a0aa4b0487c45d19a83dc2aaf
Gio feitosa <giovanifeitosa@gmail.com>
2021-12-08 16:30:35 -0300

    Navegação home-crud

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
+import { EquipementsCrudComponent } from './views/equipements-crud/equipements-crud.component';
+import { HomeComponent } from './views/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"",
+    component: HomeComponent
+  },
+  {
+    path: "equipements",
+    component: EquipementsCrudComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
