a89d06cf58d26e69acc4431bac55ec612255f21e
Gio feitosa <giovanifeitosa@gmail.com>
2021-12-08 16:02:14 -0300

    Criado o views

---
+++ frontend/src/app/views/equipements-crud/equipements-crud.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-equipements-crud',
+  templateUrl: './equipements-crud.component.html',
+  styleUrls: ['./equipements-crud.component.css']
+})
+export class EquipementsCrudComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
